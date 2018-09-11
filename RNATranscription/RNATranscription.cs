277f4f5c17b10984ffc3533bf633980b063135f9
soumya-ramteke <soumya.r@cgi.com>
2018-09-11 14:39:14 +0530

    3rd assgnmnt .net

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,33 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+            String RNA="";
+            int len = nucleotide.Length;
+            for (int i=0; i<len; i++)
+            {
+                if(nucleotide[i]=='G')
+                {
+                    RNA=RNA+"C";
+                }
+                else if(nucleotide[i]=='C')
+                {
+                     RNA=RNA+"G";
+                }
+                else if(nucleotide[i]=='T')
+                {
+                     RNA=RNA+"A";
+                }
+                else if(nucleotide[i]=='A')
+                {
+                     RNA=RNA+"U";
+                }
+                else if(nucleotide[i]=='U')
+                {
+                     RNA=RNA+"A";
+                }
+            }
+        return(RNA);
         }
     }
 }
